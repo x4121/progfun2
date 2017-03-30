2003a0bf735be99be65160dac7f8d0efbd82cbba
Armin Grodon <me@armingrodon.de>
2017-03-30 15:59:36 +0200

    Implement sum and max

--- example/src/main/scala/example/Lists.scala
+++ example/src/main/scala/example/Lists.scala
@@ -1,5 +1,6 @@
 package example
 
+import scala.annotation.tailrec
 
 object Lists {
 
@@ -8,35 +9,32 @@ object Lists {
    * multiple techniques that can be used for implementing this method, and
    * you will learn during the class.
    *
-   * For this example assignment you can use the following methods in class
-   * `List`:
-   *
-   *  - `xs.isEmpty: Boolean` returns `true` if the list `xs` is empty
-   *  - `xs.head: Int` returns the head element of the list `xs`. If the list
-   *    is empty an exception is thrown
-   *  - `xs.tail: List[Int]` returns the tail of the list `xs`, i.e. the the
-   *    list `xs` without its `head` element
-   *
-   *  ''Hint:'' instead of writing a `for` or `while` loop, think of a recursive
-   *  solution.
-   *
    * @param xs A list of natural numbers
    * @return The sum of all elements in `xs`
    */
-    def sum(xs: List[Int]): Int = ???
-  
+    def sum(xs: List[Int]): Int = {
+      @tailrec
+      def sum(xs: List[Int], acc: Int): Int = xs match {
+        case List() => acc
+        case head :: tail => sum(tail, acc + head)
+      }
+      sum(xs, 0)
+    }
+
   /**
    * This method returns the largest element in a list of integers. If the
    * list `xs` is empty it throws a `java.util.NoSuchElementException`.
-   *
-   * You can use the same methods of the class `List` as mentioned above.
-   *
-   * ''Hint:'' Again, think of a recursive solution instead of using looping
-   * constructs. You might need to define an auxiliary method.
    *
    * @param xs A list of natural numbers
    * @return The largest element in `xs`
    * @throws java.util.NoSuchElementException if `xs` is an empty list
    */
-    def max(xs: List[Int]): Int = ???
+    def max(xs: List[Int]): Int = {
+      @tailrec
+      def max(xs: List[Int], cur: Int): Int = xs match {
+        case List() => cur
+        case head :: tail => max(tail, Math.max(head, cur))
+      }
+      max(xs.tail, xs.head)
+    }
   }
